2d6267737292b05451a8f0360c9b95ab1d721474
heng.zhang <heng.zhang92@outlook.com>
2020-04-02 21:20:30 +0200

    update to have encoded data

--- MachinelearningClass/exploringData.py
+++ MachinelearningClass/exploringData.py
@@ -2,5 +2,25 @@ import numpy as np
 import sklearn
 import scipy as sp
 import pandas as pd
+import matplotlib
+import seaborn as sns
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+desired_width = 320
+pd.set_option('display.width', desired_width)
 from sklearn.datasets import load_breast_cancer
-breast_cancer_dataset=load_breast_cancer()
+titanic_df=pd.read_csv('titanic/train.csv')
+titanic_df.head(10)
+titanic_df.drop(['PassengerId','Name','Ticket','Cabin'],'columns',inplace=True)
+titanic_df.dropna(inplace=True)
+fig,ax = plt.subplots(figsize=(12,8))
+plt.scatter(titanic_df['Fare'], titanic_df['Survived'])
+plt.xlabel('Age')
+plt.ylabel('Survived')
+fig,ax = plt.subplots(figsize=(12,10))
+titanic_data_corr =titanic_df.corr()
+sns.heatmap(titanic_data_corr,annot=True)
+
+label_encoding=preprocessing.LabelEncoder()
+titanic_df['Sex']=label_encoding.fit_transform(titanic_df['Sex'])
--- investment/strategydecision.py
+++ investment/strategydecision.py
@@ -2,10 +2,12 @@ import scipy.stats as stats
 import researchpy as rp
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib
 matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
 df = pd.read_pickle('dataframe.pkl')
 df=df[df['totalasset']>30]
-result=df.pivot_table(index=['buy_tresh','sell_tresh','buysellratio'], aggfunc='size')
-result=result[result==4]
+result=df.pivot_table(index=['buysellratio','buy_tresh','sell_tresh'], aggfunc='size')
+result=result[result==result.max()]
 plt.hist([i[0] for i in result.keys()])
